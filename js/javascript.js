7f126c10ce36a227df724e2f057c2e7a18f67f1a
Eddishot <Eddi_sl94@hotmail.it>
2021-10-12 18:21:31 +0200

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -4,4 +4,11 @@ let scacchiera = document.getElementById("container-scacchiera");
 for (let i=0 ; i <= 100 ; i++){
 
     const rowCols = document.querySelector(".row-cols-7")
-}
+
+
+    rowCols.innerHTML += `  <div>
+                                <div class="ratio ratio-1x1 bg-success">
+                                    <div></div>
+                                </div>
+                            </div> `
+} 
