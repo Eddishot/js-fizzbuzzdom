0df9cb71bcd58443248188421c36730599adefd2
Eddishot <Eddi_sl94@hotmail.it>
2021-10-12 19:29:10 +0200

    exercise completed

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,41 @@
 
 let scacchiera = document.getElementById("container-scacchiera");
 
-for (let i=0 ; i <= 100 ; i++){
+for (let i=1 ; i < 100 ; i++){
 
     const rowCols = document.querySelector(".row-cols-7")
+    // inserimento variabili  i diversi multipli
+    let backgroundColor = "bg-primary";
+    let text = i;
+
+    const multiploDi3 = i % 3 === 0
+    const multiploDi5 = i % 5 === 0
+
+    if (multiploDi3){
+        backgroundColor = "bg-success"
+
+        text = "fizz"
+
+    }
 
 
-    rowCols.innerHTML += `  <div class="p-2">
-                                <div class="ratio ratio-1x1 bg-success border-2">
-                                    <div></div>
+    if (multiploDi5){
+        backgroundColor = "bg-warning"
+        text = "buzz"
+    }
+
+
+    if (multiploDi3 && multiploDi5){
+        backgroundColor = "bg-danger"
+        text = "fizzbuzz"
+    }
+     
+    
+
+
+    rowCols.innerHTML += `  <div class="p-1">
+                                <div class="ratio ratio-1x1 ${backgroundColor} ">
+                                    <div class="d-flex justify-content-center align-items-center fs-4">${text}</div>
                                 </div>
                             </div> `
 
